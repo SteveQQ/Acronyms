dab4c26eb7ff2612da6cf4fa5cd75a8aa4ab2338
SteveQ <steveq@dot.com>
2016-08-02 18:37:45 +0200

    first commit

---
+++ src/GUI.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by SteveQ on 2016-08-01.
+ */
+public class GUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Acronyms Gnerator");
+        JPanel panelGlowny = new JPanel();
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+}
