890cd75041035a1085f3b8d75149b2eab3a65dd8
SteveQ <steveq@dot.com>
2016-08-02 21:49:29 +0200

    textInputArea added

--- src/GUI.java
+++ src/GUI.java
@@ -1,16 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by SteveQ on 2016-08-01.
  */
 public class GUI {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Acronyms Gnerator");
-        JPanel panelGlowny = new JPanel();
-
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
-        frame.setVisible(true);
+        new GUI().createGUI();
     }
 
+    public void createGUI(){
+        JFrame frame = new JFrame("Acronyms Gnerator");
+        JPanel mainPanel = new JPanel();
+        JPanel wrappingPanel = new JPanel();
+        JTextArea inputWords = new JTextArea(10, 40);
+        JScrollPane textInputScrollPane = new JScrollPane(inputWords, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        wrappingPanel.setLayout(new BoxLayout(wrappingPanel, BoxLayout.Y_AXIS));
+
+        inputWords.setLineWrap(true);
+
+        wrappingPanel.add(textInputScrollPane);
+        mainPanel.add(wrappingPanel);
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(700,700);
+        frame.setVisible(true);
+    }
 }
